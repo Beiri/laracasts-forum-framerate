2adb2beba35ccc55dc1a7f92e5a35950e921f313
Beiri Alife <beiri.al@outlook.com>
2024-07-07 17:07:11 -0300

    Ep 11 - Linking Up the Index

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -3,12 +3,20 @@
         <AppLayout>
             <Container>
                 <ul class="divide-y">
-                    <li
-                        v-for="post in posts.data"
-                        :key="post.id"
-                        class="px-2 py-4"
-                    >
-                        <span class="font-bold text-lg">{{ post.title }}</span>
+                    <li v-for="post in posts.data" :key="post.id">
+                        <Link
+                            :href="route('posts.show', post.id)"
+                            class="group px-2 py-4 block"
+                        >
+                            <span
+                                class="font-bold text-lg group-hover:text-indigo-500"
+                                >{{ post.title }}</span
+                            >
+                            <span class="block pt-1 text-sm text-gray-600"
+                                >{{ formattedDate(post) }} ago by
+                                {{ post.user.name }}</span
+                            >
+                        </Link>
                     </li>
                 </ul>
 
@@ -22,8 +30,14 @@
 import AppLayout from "@/Layouts/AppLayout.vue";
 import Container from "@/Components/Container.vue";
 import Pagination from "@/Components/Pagination.vue";
+import { Link } from "@inertiajs/vue3";
+import { formatDistance, parseISO } from "date-fns";
 
 defineProps(["posts"]);
+
+const formattedDate = (post) => {
+    return formatDistance(parseISO(post.created_at), new Date());
+};
 </script>
 
 <style lang="scss" scoped></style>
